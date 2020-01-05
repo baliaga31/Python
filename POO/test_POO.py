24aeefa77875c2a74cf7298b5732471cf3cf9f72
wanaga3166 <benoit-aliaga@orange.fr>
2020-01-05 18:42:22 +0100

    First script in POO

---
+++ POO/test_POO.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Point:
+    def __init__(self):
+        self.x = 1
+        self.y = 1
+
+a = Point()
+print("a : x =", a.x, "y =", a.y)
---
+++ POO/test_POO_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Citron:
+    couleur = "jaune"
+
+# Main programme
+
+if __name__ == "__main__":
+    citron1 = Citron()
+    print(citron1.couleur)
+    citron2 = Citron()
+    print(citron2.couleur)
