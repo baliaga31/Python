dadaca787e5879b539935db921c837d6a7c19562
wanaga3166 <benoit-aliaga@orange.fr>
2020-01-07 17:39:58 +0100

    Second exercice

--- Python_pour_sciences_de_la_vie/Chapitre_4/Chapitre_4.py
+++ Python_pour_sciences_de_la_vie/Chapitre_4/Chapitre_4.py
@@ -4,8 +4,21 @@
 
 Jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
-
 print(Jours[0:5])
 print(Jours[6:7])
 print(Jours[-7:-2])
 print(Jours[-2:])
+
+# Exercice 4.2: Saisons
+
+Hivers = ["Janvier", "Fevrier", "Mars"]
+Printemps = ["Avril", "Mai", "Juin"]
+Ete = ["Juillet", "Aout", "Septembre"]
+Automne = ["Octobre", "Novembre", "Decembre"]
+
+saisons = [Hivers, Printemps, Ete, Automne]
+
+print(saisons[2])
+print(saisons[1][0])
+print(saisons[1:2])
+print(saisons[:][1])
