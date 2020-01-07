c2432395d59a162db8a720a40609080634eb50dd
wanaga3166 <benoit-aliaga@orange.fr>
2020-01-07 17:26:35 +0100

    First exercice

---
+++ Python_pour_sciences_de_la_vie/Chapitre_4/Chapitre_4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Exercice 4.1: Jours de la semaine
+
+Jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
+
+print(Jours[0:5])
+print(Jours[6:7])
+print(Jours[-7:-2])
+print(Jours[-2:])
