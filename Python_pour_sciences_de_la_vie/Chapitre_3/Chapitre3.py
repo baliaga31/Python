287d50d0d85c4ad5b00d91e37c8447ed193674d0
wanaga3166 <benoit-aliaga@orange.fr>
2020-01-07 10:25:09 +0100

    exercice 3.4 and I change the file name test.py to Chapitre3.py

--- Python_pour_sciences_de_la_vie/Chapitre_3/Chapitre3.py
+++ Python_pour_sciences_de_la_vie/Chapitre_3/Chapitre3.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
-# Poly-A
+# Exercice 3.2: Poly-A
 
 a = "A" * 20
 
 print(a)
 
-# Poly-A et poly-GC
+# Exercice 3.3: Poly-A et poly-GC
 
 b = "A" * 20 + "GC" * 20
 
 print(b)
+
+# Exercice 3.4: Ecriture formatee
+
+a = "Salut"
+b = 102
+c = 10.318
+
+print("{:s}, {:d}, {:.2f}".format(a, b, c))
