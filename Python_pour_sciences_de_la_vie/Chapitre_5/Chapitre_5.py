1f7026d350609a9ba2573d94a97e3918dd779a6d
wanaga3166 <benoit-aliaga@orange.fr>
2020-01-16 19:49:09 +0100

    Exercice 5.5

--- Python_pour_sciences_de_la_vie/Chapitre_5/Chapitre_5.py
+++ Python_pour_sciences_de_la_vie/Chapitre_5/Chapitre_5.py
@@ -22,3 +22,52 @@ i = 0
 while i < len(x):
     print(x[i])
     i = i + 1
+
+
+# Exercice 5.2: Boucle et jours de la semaine
+
+jours = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+
+print("Solution 1")
+
+for day in jours[0:5]:
+    print("Les jours de la semaine sont {}.".format(day))
+
+print("Solution 2")
+
+for day in jours[5:7]:
+    print("Les jours du week-end sont {}.".format(day))
+
+print("Solution 3")
+
+i = 5
+while i <= 6:
+    print("les jours du week-end sont {}.".format(jours[i]))
+    i = i +1
+
+# Exercice 5.3: Nombres de 1 a 10 sur une ligne
+
+for n in range(1, 11):
+    print(n, end=" ")
+
+
+# Exercice 5.4: Nombres pairs et impairs
+
+impairs = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+pair = []
+for z in impairs:
+    p = z + 1
+    print(p)
+    pair.append(p) # Alternative: pair.append(n+1)
+
+
+# Exercice 5.5: Calcul de la moyenne
+
+notes = [14, 9, 6, 8, 12]
+somme = 0
+
+for y in notes:
+    somme = somme + y
+
+moyenne = somme/len(notes)
+print("Cet etudiant a une moyenne de {:.2f}.".format(moyenne))
