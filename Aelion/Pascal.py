2de7c97153a6badacfe30e2c73c7540593160b25
wanaga3166 <benoit-aliaga@orange.fr>
2020-06-28 09:22:26 +0200

    Ajout commentaires

--- Aelion/Pascal.py
+++ Aelion/Pascal.py
@@ -1,11 +1,11 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
 def pascal_triangle(rang):
-    for n in range(rang+1):
+    for n in range(rang+1): # On explore les lignes
         if n == 0:
             print(1)
         else:
-            for k in range(rang+1):
+            for k in range(rang+1): # On explore les colonnes
                  if k == 0:
                      print(1, 1)
                  else:
